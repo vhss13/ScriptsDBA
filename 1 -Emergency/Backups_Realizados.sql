0056f2ddc45dd13431807a8060efca9ea0802eae
vhss13 <97907322+vhss13@users.noreply.github.com>
2023-01-08 12:58:52 -0300

    Update Backups_Realizados.sql

--- 1 -Emergency/Backups_Realizados.sql
+++ 1 -Emergency/Backups_Realizados.sql
@@ -1,8 +1,14 @@
---Listar os últimos backups realizados com SUCESSO
+--Listar os Ãºltimos backups realizados com SUCESSO
 SELECT  sd.name,
         CASE bs.TYPE
-		WHEN 'D' THEN 'DADOS'
-		WHEN 'L' THEN 'LOG'
+			WHEN 'D' THEN 'database'
+			WHEN 'L' THEN 'log'
+			WHEN 'I' THEN 'differential'
+			WHEN 'F' THEN 'filegroup'
+			WHEN 'G' THEN 'diff file'
+			WHEN 'P' THEN 'partial'
+			WHEN 'Q' THEN 'partial differential'
+			ELSE 'other'
 		END AS Type,
         bs.database_name,
         max(bs.backup_start_date) as last_backup
