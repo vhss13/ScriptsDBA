3a101c024b80dbd81e756e8aa9e34cd0afb04d85
vhss13 <97907322+vhss13@users.noreply.github.com>
2023-02-17 13:41:11 -0300

    Update DBA tamanho e utilizacao de arquivos.sql

--- 5 - Administrativos/DBA tamanho e utilizacao de arquivos.sql
+++ 5 - Administrativos/DBA tamanho e utilizacao de arquivos.sql
@@ -1,12 +1,12 @@
 SELECT
+CAST(100 * (CAST (((sysfiles.size/128.0 -CAST(FILEPROPERTY(sysfiles.name,
+ + '' + 'SpaceUsed' + '' + '' ) AS int)/128.0)/(sysfiles.size/128.0))
+AS decimal(4,2))) AS varchar(8)) + '' + '' + '%' + '' + '' AS 'FreeSpace%',
 CAST(sysfiles.size/128.0 AS int) AS FileSizeMB,
+CAST(sysfiles.size/128.0 - CAST(FILEPROPERTY(sysfiles.name,  + '' +
+       'SpaceUsed' + '' + '' ) AS int)/128.0 AS int) AS FreeSpaceMB,
 sysfiles.name AS LogicalFileName, sysfiles.filename AS PhysicalFileName,
 CONVERT(sysname,DatabasePropertyEx('TempDB','Status')) AS Status,
 CONVERT(sysname,DatabasePropertyEx('TempDB','Updateability')) AS Updateability,
-CONVERT(sysname,DatabasePropertyEx('TempDB','Recovery')) AS RecoveryMode,
-CAST(sysfiles.size/128.0 - CAST(FILEPROPERTY(sysfiles.name,  + '' +
-       'SpaceUsed' + '' + '' ) AS int)/128.0 AS int) AS FreeSpaceMB,
-CAST(100 * (CAST (((sysfiles.size/128.0 -CAST(FILEPROPERTY(sysfiles.name,
- + '' + 'SpaceUsed' + '' + '' ) AS int)/128.0)/(sysfiles.size/128.0))
-AS decimal(4,2))) AS varchar(8)) + '' + '' + '%' + '' + '' AS 'FreeSpace%'
+CONVERT(sysname,DatabasePropertyEx('TempDB','Recovery')) AS RecoveryMode
 FROM dbo.sysfiles
